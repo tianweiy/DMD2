87e42cd5f4d15ff3b7e491ae3dfa4576b2a04baf
Tianwei Yin <tianweiy@mit.edu>
2024-05-28 19:31:02 -0700

    fix typo

--- scripts/download_sdxl_ode_pair_10k_lmdb.sh
+++ scripts/download_sdxl_ode_pair_10k_lmdb.sh
@@ -1,5 +1,5 @@
 $CHECKPOINT_PATH=$1
 
-wget https://huggingface.co/tianweiy/DMD2/resolve/main/data/laion/sdxl_ode_pair_10k_lmdb.zip?download=true -O $CHECKPOINT_PATH/sdxl_ode_pair_10k_lmdb.zip sdxl_ode_pair_10k_lmdb.zip 
+wget https://huggingface.co/tianweiy/DMD2/resolve/main/data/laion/sdxl_ode_pair_10k_lmdb.zip?download=true -O $CHECKPOINT_PATH/sdxl_ode_pair_10k_lmdb.zip
 
 unzip $CHECKPOINT_PATH/sdxl_ode_pair_10k_lmdb.zip -d $CHECKPOINT_PATH

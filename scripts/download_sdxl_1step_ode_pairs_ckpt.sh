036d4292248c62b4868843173b24e708abf9a20e
Tianwei Yin <tianweiy@mit.edu>
2024-06-05 10:38:40 -0700

    Update download_sdxl_1step_ode_pairs_ckpt.sh

--- scripts/download_sdxl_1step_ode_pairs_ckpt.sh
+++ scripts/download_sdxl_1step_ode_pairs_ckpt.sh
@@ -1,3 +1,3 @@
-$CHECKPOINT_PATH=$1
+CHECKPOINT_PATH=$1
 
 wget https://huggingface.co/tianweiy/DMD2/resolve/main/model/sdxl/sdxl_lr1e-5_8node_ode_pretraining_10k_cond399_checkpoint_model_002000.bin?download=true -O $CHECKPOINT_PATH/sdxl_lr1e-5_8node_ode_pretraining_10k_cond399_checkpoint_model_002000.bin
